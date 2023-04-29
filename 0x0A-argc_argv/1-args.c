c6eaaa8e4715f83a2bcd97e42985f4d974207ff0
= <=>
2023-04-29 02:46:30 +0200

    new start

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,8 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/
-		printf("%d\n", argc - 1);
-
-		return (0);
+	(void)argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+	return (0);
 }
